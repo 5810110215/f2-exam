f791958f970595ba89cf0ac0eaa86c0f8242ff37
5810110215 <piyaphat_paiy@hotmail.com>
2020-05-09 13:44:32 +0700

    initial commit

--- Ui.java
+++ Ui.java
@@ -8,7 +8,7 @@ import javax.swing.JTextField;
 
 
 public class Ui {
-    private static final String[] FACES = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"};
+
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Calculate");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,16 +32,17 @@ public class Ui {
 		frame.add(facesPanel, BorderLayout.CENTER);
 		
         JButton button = new JButton("Cal");
-        for(String face : FACES){
-			facesPanel.add(new JButton(face));
-		}
+
+     
 
 		textField.setFont(textField.getFont().deriveFont(24.0f));
 		frame.add(textField, BorderLayout.PAGE_START);
 
     
 		button.setPreferredSize(new Dimension(100, 50));
-		frame.add(button, BorderLayout.PAGE_END);
+        frame.add(button, BorderLayout.CENTER  );
+        JTextField textField1 = new JTextField("KEY");
+        frame.add(textField1, BorderLayout.PAGE_END);
 		
 		frame.setVisible(true);
 	}
